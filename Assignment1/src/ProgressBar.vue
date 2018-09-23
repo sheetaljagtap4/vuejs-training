e4f0bb7324a2edb4fc2ef29299aa7a45c94d1f22
Sheetal Jagtap <sheetal.jagtap@globant.com>
2018-09-23 21:41:14 +0530

    Todo

---
+++ Assignment1/src/ProgressBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row">
+        <div class="col-md-12 mdiv">
+            <div v-bind:style="getdivwidth()">
+                <div class="progressBar">
+                    {{Itemlist.length}}/12
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['Itemlist'],
+        methods:{
+            getdivwidth: function(){
+            return {
+                'width': (this.Itemlist.length*8.33) + '%',
+                'background-color': 'orange'
+            };
+          }
+        }
+    };
+</script>
+
+<style>
+    .progressBar{
+        width: 100%;
+        text-align: center;
+        padding: 5px;
+    }
+</style>
